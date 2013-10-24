99727b14f398b446ac21a523d195de74ddd0f9e6
Christopher Smith <chris@x86-64.info>
2013-10-25 00:50:08 +0900

    post format ready

--- drpepper/twitter.py
+++ drpepper/twitter.py
@@ -2,6 +2,8 @@
 
 import yaml
 import tweepy
+from datetime import datetime
+from string import Template
 
 def prepare_api():
 	keys = yaml.load(open("config.yml"))['oauth']
@@ -11,15 +13,20 @@ def prepare_api():
 
 	return tweepy.API(auth_handler = oauth, secure = True, retry_count = 3)
 
+def post(item, price):
+	now = datetime.now().strftime("%Y/%m/%d %H:%M")
+	status = Template(
+		u'${date_and_time} 現在の ${item} の価格: ${price} (前日比 ${diff}) ${url}'
+		).substitute(
+			{'date_and_time': now, 'item': item, 'price': price, 'diff': 0, 'url': 'nanka'}
+		)
+	return status
+
 def test():
 	api = prepare_api()
+	print post(u'なんか', u'￥ 100')
 	try:
 		print api.me().__dict__
-	except:
-		raise
-
-	try:
-		api.update_status(u'てすてす')
 	except:
 		raise
 
