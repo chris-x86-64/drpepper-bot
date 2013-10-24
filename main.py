0da57c9ab9816b41b78376caf8b8879ca72d8ed6
Christopher Smith <chris@x86-64.info>
2013-10-24 23:54:05 +0900

    removed twitter stuff from main.py

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-import yaml
-
-config = yaml.load(open("config.yml"))
-
-class Keys(object):
-	class consumer(object):
-		key = config['oauth']['consumer']['key']
-		secret = config['oauth']['consumer']['secret']
-	class access_token(object):
-		key = config['oauth']['access_token']['key']
-		secret = config['oauth']['access_token']['secret']
+import drpepper.scraper
+import drpepper.twitter
 
 def main():
-	print Keys.consumer.key
+	print "There's nothing really to do."
+
+def test():
+	print "I'm sleepy"
 
 if __name__ == '__main__':
-	main()
+	test()
